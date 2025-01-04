b580a25b5a8e78d487ad2bb5870ace0dd06a241d
marina22 <marina20471@gmail.com>
2025-01-04 18:40:29 +0300

    made changes two

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+
+type MyComponentProType = {text: string}
+
+
+const MyComponent: FC <MyComponentProType> =  ({text}) =>{
+    return <div className= 'text-3xl font-bold underlin'>
+        {text}
+
+            </div>
+}
+export default MyComponent;
